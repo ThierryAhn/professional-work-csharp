d575f5c4458aab2470d57a6381ebf419873c5ff9
folabi41@gmail.com <folabi41@gmail.com@08328662-f717-35d1-e1c7-779e3d396022>
2014-09-24 20:08:49 +0000

    initial commit

---
+++ trunk/KSAEvenementiel/Controllers/ServiceController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KSAEvenementiel.Controllers
+{
+    public class ServiceController : Controller
+    {
+        //
+        // GET: /Service/
+
+        /* public ActionResult Index()
+        {
+            return View();
+        } */
+
+        //
+        // GET: /Service/Billetterie
+
+        public ActionResult Billetterie()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Service/Bureau
+
+        public ActionResult Bureau()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Service/Evenement
+
+        public ActionResult Evenement()
+        {
+            return View();
+        }
+
+
+        //
+        // GET: /Service/Materiel
+
+        public ActionResult Materiel()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Service/Salle
+
+        public ActionResult Salle()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Service/Vente
+
+        public ActionResult Vente()
+        {
+            return View();
+        }
+    }
+}
